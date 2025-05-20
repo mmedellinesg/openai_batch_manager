757cc1c53a80f684e4398210bf8ede4d060c30b9
mmedellinesg <mmedellinesg@gmail.com>
2025-05-20 14:07:34 -0400

    Add jsonl_filewriter.py script

---
+++ jsonl_filewriter.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import json
+
+def write_jsonl_file_from_df(
+    df, 
+    system,
+    col,
+    model = "gpt-4.1-mini",
+    max_tokens = 4096):
+    # Write JSONL file
+    jsonl_content = ""
+    i = 1
+    for idx, entry in df.iterrows():
+        jsonl_content += write_jsonl_row(f"request-{i}", model, system, entry[col], max_tokens)
+        i += 1
+    return jsonl_content
+
+
+def write_jsonl_row(custom_id, model, system, user_input, max_tokens=4096):
+    # Create a single JSONL row
+    line = {
+        "custom_id": custom_id,
+        "method": "POST",
+        "url": "/v1/chat/completions",
+        "body": {
+            "model": model,
+            "messages": [
+                {"role": "system", "content": system},
+                {"role": "user", "content": user_input}
+            ],
+            "max_tokens": max_tokens
+        }
+    }
+    return json.dumps(line) + "\n"
